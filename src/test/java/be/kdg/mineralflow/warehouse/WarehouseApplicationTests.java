5a7cfcb12c7d0d391e0d913e5b8de66dfbd48cc7
H-iver <insaf.hichour@student.kdg.be>
2024-09-21 17:07:16 +0200

    Initial commit

---
+++ src/test/java/be/kdg/mineralflow/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.mineralflow.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
