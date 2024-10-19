753d698c2055979c4e9b5f56db5ef46f9f1139a1
Timon Van Puyenbroeck <timon.vanpuyenbroeck@student.kdg.be>
2024-10-19 16:47:13 +0200

    sql inserts: maxcapacity added to warehouse 2

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -24,12 +24,13 @@ DO
         INSERT INTO resource (id, name, selling_price_per_ton, storage_price_per_ton_per_day)
         VALUES (''22222222-2222-2222-2222-222222222223'', ''Goud'', 5.0, 1.0);
 
-        INSERT INTO warehouse (id, resource_id, vendor_id, used_capacity_in_ton, warehouse_number)
+        INSERT INTO warehouse (id, resource_id, vendor_id, used_capacity_in_ton, warehouse_number,max_capacity_in_ton)
         VALUES (''22222222-2222-2222-2222-222222222224'',
             ''22222222-2222-2222-2222-222222222223'', -- resource_id (UUID of related resource entity)
             ''22222222-2222-2222-2222-222222222222'',
             0,
-            3);
+            3,
+warehouseMaxCapacityInTon);
     END
 '
 ;
