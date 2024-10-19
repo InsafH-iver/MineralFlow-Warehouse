051d93b1fb9f8779717f02ab8c8af51d298e6484
Timon Van Puyenbroeck <timon.vanpuyenbroeck@student.kdg.be>
2024-10-19 16:31:13 +0200

    warehousemxcapacity added to tests

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -22,14 +22,14 @@ DO
 
         -- Resource Table
         INSERT INTO resource (id, name, selling_price_per_ton, storage_price_per_ton_per_day)
-        VALUES (''22222222-2222-2222-2222-222222222223'', ''Goud'', 5.0,1.0);
+        VALUES (''22222222-2222-2222-2222-222222222223'', ''Goud'', 5.0, 1.0);
 
         INSERT INTO warehouse (id, resource_id, vendor_id, used_capacity_in_ton, warehouse_number)
         VALUES (''22222222-2222-2222-2222-222222222224'',
-                ''22222222-2222-2222-2222-222222222223'', -- resource_id (UUID of related resource entity)
-                ''22222222-2222-2222-2222-222222222222'',
-                0,
-                3);
+            ''22222222-2222-2222-2222-222222222223'', -- resource_id (UUID of related resource entity)
+            ''22222222-2222-2222-2222-222222222222'',
+            0,
+            3);
     END
 '
 ;
